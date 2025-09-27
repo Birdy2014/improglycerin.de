53d7ce73f1ee861fa877d515a7a94f1db2fdb246
Moritz Vogel <moritzv7@gmail.com>
2025-09-27 22:50:12 +0200

    Fix font styles and add some text

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,30 @@ export default function Home() {
           "https://improglycerin.de/wp-content/uploads/2019/11/improglycerin-frankfurt-improtheater-Slider19-04-1200x500.jpg",
         ]}
       />
-      <Testimonial author="Wiesbadener Tagblatt">
-        Anspruchsvolle Kleinkunst… turbulent und lustig!
-      </Testimonial>
-      <Testimonial author="RadioX">
-        Wirklich klasse, wirklich schön! Ein sehr schöner, sehr unterhaltsamer
-        Abend! Ihr arbeitet dran, dass die Improszene in Frankfurt noch
-        lebendiger wird.
-      </Testimonial>
-      <Testimonial author="Sachsenhäuser Wochenblatt">
-        Eine Improtheater-Show der besonderen Art!
-      </Testimonial>
+      <div className={styles["group-vertical"]}>
+        <h2>IMPROTHEATER AUS FRANKFURT AM MAIN</h2>
+        <p>
+          Explosiv. Intensiv. Jetzt. Ihr ruft uns Ideen zu, wir machen daraus
+          Szenen. Sofort.
+          <br />
+          Ohne Drehbuch, ohne Versatzstücke, ohne Vorbereitung. Drama, Komödie
+          und Stand-Up-Comedy in einem.
+        </p>
+        <p>Lasst Euch mitreißen. Das ist Improvisationstheater.</p>
+      </div>
+      <div className={styles["group-vertical"]}>
+        <Testimonial author="Wiesbadener Tagblatt">
+          Anspruchsvolle Kleinkunst… turbulent und lustig!
+        </Testimonial>
+        <Testimonial author="RadioX">
+          Wirklich klasse, wirklich schön! Ein sehr schöner, sehr unterhaltsamer
+          Abend! Ihr arbeitet dran, dass die Improszene in Frankfurt noch
+          lebendiger wird.
+        </Testimonial>
+        <Testimonial author="Sachsenhäuser Wochenblatt">
+          Eine Improtheater-Show der besonderen Art!
+        </Testimonial>
+      </div>
     </div>
   );
 }
