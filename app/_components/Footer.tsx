2e5a8439094d26fff5096d667dae253d4d3fa5b5
Moritz Vogel <moritzv7@gmail.com>
2025-09-29 21:04:54 +0200

    Footer: Add padding on sides

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -8,24 +8,26 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.line}></div>
-      <div className={styles.center}>
-        <div>
-          Copyright 2025 – IMPROGLYCERIN Improvisationstheater aus Frankfurt am
-          Main e.V.
-        </div>
-        <div className={styles["links-legal"]}>
-          <Link href="/datenschutz">DATENSCHUTZ</Link>
-          <Link href="/impressum">IMPRESSUM</Link>
-        </div>
-        <div className={`${styles["links-social"]} ${icomoon.className}`}>
-          <Link
-            href="https://www.facebook.com/improglycerin"
-            className={styles["icon-facebook"]}
-          ></Link>
-          <Link
-            href="https://www.instagram.com/improglycerin"
-            className={styles["icon-instagram"]}
-          ></Link>
+      <div className="center-container">
+        <div className={styles.center}>
+          <div>
+            Copyright 2025 – IMPROGLYCERIN Improvisationstheater aus Frankfurt
+            am Main e.V.
+          </div>
+          <div className={styles["links-legal"]}>
+            <Link href="/datenschutz">DATENSCHUTZ</Link>
+            <Link href="/impressum">IMPRESSUM</Link>
+          </div>
+          <div className={`${styles["links-social"]} ${icomoon.className}`}>
+            <Link
+              href="https://www.facebook.com/improglycerin"
+              className={styles["icon-facebook"]}
+            ></Link>
+            <Link
+              href="https://www.instagram.com/improglycerin"
+              className={styles["icon-instagram"]}
+            ></Link>
+          </div>
         </div>
       </div>
     </footer>
